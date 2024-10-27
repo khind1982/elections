0d9eb2b25085a96d01755301cf3080bdaed9cde5
khind <kevinhind2000@yahoo.co.uk>
2024-10-27 01:01:42 +0000

    Code to add details page for each constituency.

--- election_gen2024_essex/election_results/urls.py
+++ election_gen2024_essex/election_results/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('results/', views.results, name='results'),
+    path('results/details/<int:id>', views.details, name='details')
 ]
--- election_gen2024_essex/election_results/views.py
+++ election_gen2024_essex/election_results/views.py
@@ -3,10 +3,17 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import GEResults
 
+# Create your views here.
+
 def results(request):
     election_results = GEResults.objects.all().values()
     template = loader.get_template('results.html')
     context = {'election_results': election_results}
     return HttpResponse(template.render(context, request))
 
-# Create your views here.
+
+def details(request, id):
+    result = GEResults.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {'result': result}
+    return HttpResponse(template.render(context, request))
